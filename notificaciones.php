a82d5ca04d454de8e06aa9fc10f6bde0c5886f62
guillermorag <guillermoragone@gmail.com>
2020-06-13 16:08:10 -0300

    notiget

--- notificaciones.php
+++ notificaciones.php
@@ -8,12 +8,14 @@ print_r($_POST);
 echo("</br>");
 */
 
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+
+
 if (isset($_POST["type"])){
     header('Content-Type: application/json');
     echo json_encode(['HTTP/1.1 200 OK'], 200);
 
-       MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-
+      
        switch($_POST["type"]) {
            case "payment":
                $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
@@ -35,8 +37,40 @@ if (isset($_POST["type"])){
        }
     }
     else {
-        header('Content-Type: application/json');
-       echo json_encode(['HTTP/1.1 500 ERROR'], 500);
+      //  header('Content-Type: application/json');
+      // echo json_encode(['HTTP/1.1 500 ERROR'], 500);
     }
+
+
+
+    if (isset($_GET["type"])){
+        header('Content-Type: application/json');
+        echo json_encode(['HTTP/1.1 200 OK'], 200);
+    
+          
+           switch($_POST["type"]) {
+               case "payment":
+                   $payment = MercadoPago\Payment.find_by_id($_POST["id"]);
+                   $archivo = fopen("datos.txt","w+b");  
+                        if( $archivo ) {
+                            file_put_contents("datos.txt", $payment);
+                        }
+                            fclose($archivo);   
+                   break;
+               case "plan":
+                   $plan = MercadoPago\Plan.find_by_id($_POST["id"]);
+                   break;
+               case "subscription":
+                   $plan = MercadoPago\Subscription.find_by_id($_POST["id"]);
+                   break;
+               case "invoice":
+                   $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
+                   break;
+           }
+        }
+        else {
+          //  header('Content-Type: application/json');
+          // echo json_encode(['HTTP/1.1 500 ERROR'], 500);
+        }
 
 ?>       
