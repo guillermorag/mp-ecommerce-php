37578854e05237bfd3be63e74c2b4bad6d050e62
guillermorag <guillermoragone@gmail.com>
2020-06-13 13:10:45 -0300

    nose

--- mp.php
+++ mp.php
@@ -6,15 +6,8 @@
 
 $_SERVIDOR = $_SERVER["HTTP_HOST"];
 
-define ('SUB','/mp-ecommerce-php/');
-/*
-define ('URL_SUCESS','https://'.$_SERVIDOR.SUB.'mpsucess.php');
-define ('URL_FAILURE','https://'.$_SERVIDOR.SUB.'mpfailure.php');
-define ('URL_PENDING','https://'.$_SERVIDOR.SUB.'mppending.php');
+//define ('SUB','/mp-ecommerce-php/');
 
-define ('URL_NOTIFICACIONES','https://'.$_SERVIDOR.SUB.'n.php');
-
-define ('URL_IMG','https://'.$_SERVIDOR.SUB.'assets');*/
 
 define ('URL_SUCESS','https://'.$_SERVIDOR.'mpsucess.php');
 define ('URL_FAILURE','https://'.$_SERVIDOR.'mpfailure.php');
@@ -27,16 +20,12 @@ define ('URL_IMG','https://'.$_SERVIDOR.'assets');
 
 
 
-
-
-
 // SDK de Mercado Pago
 require __DIR__ .  '/vendor/autoload.php';
 
 // Agrega credenciales
 
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 
@@ -71,8 +60,7 @@ function crear_preferencia  ($art,$pagador){
        
     $imagen = path_image($art["img"]);
 
-    echo ("este es el la imagen ". $imagen."<br>");
-   // echo ("este es el serimagenesvidor ". $art["img"]);
+    
      
 
     $item = new MercadoPago\Item();
@@ -124,24 +112,15 @@ function crear_preferencia  ($art,$pagador){
 
 function path_image($img){
 
-    //solo ejemplo prueba arreglar para devolverl el path correcto
+    
     
    $r = str_replace ('./assets',"",$img );
    return URL_IMG.$r;  
     
-  // return "https://".$host.$url.$img;
+
 
 }
 
-function get_file_dir() {
-    global $argv;
-    $dir = dirname(getcwd() . '/' . $argv[0]);
-    $curDir = getcwd();
-    chdir($dir);
-    $dir = getcwd();
-    chdir($curDir);
-    return $dir;
-}
 
 
 
