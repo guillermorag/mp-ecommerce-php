64b8d9df6ef4b4d485ce13da4641fbaedb5a5f3b
guillermorag <guillermoragone@gmail.com>
2020-06-14 14:49:59 -0300

    vererr

--- notificaciones.php
+++ notificaciones.php
@@ -69,10 +69,10 @@ if (isset($_POST["type"])){
                   $data =  file_get_contents('https://api.mercadopago.com/v1/payments/:'.$_GET["data_id"].'?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
                 //  $a = json_decode($data,true);
 
-               // echo ("<br>");
-                 // print_r(json_encode($payment));
-                 // print_r(json_encode($data))
-               // echo("</br>"); 
+                echo ("<br>");
+                  print_r(json_encode($payment));
+                  print_r(json_encode($data))
+                echo("</br>"); 
                  
                                
                
