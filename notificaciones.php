472396e7a387933dbef372578936fb8c2f727127
guillermorag <guillermoragone@gmail.com>
2020-06-14 14:23:15 -0300

    pid

--- notificaciones.php
+++ notificaciones.php
@@ -64,8 +64,9 @@ if (isset($_POST["type"])){
                case "payment":
                   $payment = MercadoPago\Payment::find_by_id($_GET["data_id"]);
                   
+                  
                  
-                  $data =  file_get_contents('https://api.mercadopago.com/v1/payments/:'.$_GET["data_id"].'?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+                  $data =  file_get_contents('https://api.mercadopago.com/v1/payments/:'.$payment->id.'?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
                   $a = json_decode($data,true);
  
                  
