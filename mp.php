b9f630c98a712232b955c3f8ed515bd59c9cb75a
guillermorag <guillermoragone@gmail.com>
2020-07-22 19:18:26 -0300

    pr

--- mp.php
+++ mp.php
@@ -26,8 +26,8 @@ define ('URL_IMG','https://'.$_SERVIDOR.'/assets');
 require __DIR__ .  '/vendor/autoload.php';
 
 // Agrega credenciales
-
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+
 //MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 
