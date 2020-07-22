8215736b77a1b95ff6f69f76af82d3c5ed11c213
guillermorag <guillermoragone@gmail.com>
2020-07-22 20:11:07 -0300

    prefjson

--- detail.php
+++ detail.php
@@ -40,7 +40,9 @@ $pagador = array (
 
 $p = crear_preferencia ($articulo,$pagador);
 
-guarda_log("Preference_id =".$p);
+guarda_log("Preference_id =".$p->id);
+guarda_log("Preference_id =".json_encode($p));
+
 /*echo ("<pre>");
 print_r ($p);
 echo ("</pre>");
