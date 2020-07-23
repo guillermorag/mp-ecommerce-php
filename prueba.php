e84659708f42190898627273293b0af424f92a00
guillermorag <guillermoragone@gmail.com>
2020-07-22 21:34:05 -0300

    pruarriba

--- prueba.php
+++ prueba.php
@@ -1,8 +1,14 @@
-<?php// get posted data
+<?php
 
-$data = json_decode(file_get_contents("php://input"));
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+$d =  json_decode(file_get_contents("php://input"));
   
-echo ($data)  
+echo ($d)  
    
 //http_response_code(200);
 
